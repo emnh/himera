36f4ab9e39b72de1af81c2429adfcab79d2f3377
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2013-08-29 20:00:46 +0200

    update dependencies

--- project.clj
+++ project.clj
@@ -9,11 +9,11 @@
 (defproject himera "0.1.0-SNAPSHOT"
   :description "ClojureScript compiler service."
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring "1.0.2"]
-                 [compojure "1.0.1"]
+                 [ring "1.2.0"]
+                 [compojure "1.1.3"]
                  [domina "1.0.1"]
                  [ring-clj-params "0.1.0"]
-                 [org.clojure/clojurescript "0.0-1847"]]
+                 [org.clojure/clojurescript "0.0-1859"]]
   :plugins [[lein-cljsbuild "0.3.2"]]
   :dev-dependencies [[jline "0.9.94"]
                      [lein-marginalia "0.7.0-SNAPSHOT"]]

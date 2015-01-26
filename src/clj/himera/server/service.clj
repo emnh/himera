41e1c584a8f8216d6832bf609bfe97b2b3fe5a50
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2015-01-26 02:11:53 +0100

    add compile-string with better error reporting. still no line numbers

--- src/clj/himera/server/service.clj
+++ src/clj/himera/server/service.clj
@@ -35,17 +35,25 @@
                                        (str 
                                             (or callback "jsonp")
                                             "("
-                                            (pr-str (string/trim-newline code))
+                                            (pr-str code)
                                             ");")))))
 
 (def generate-js-response (partial generate-response
                                    (fn [data]
                                      (let [code (or (:result data) "'HIMERA ERROR: NOTHING GENERATED'")]
-                                       (pr-str {:js (string/trim-newline code)})))))
+                                       (pr-str {:js code})))))
+
+(def generate-js-error-response (partial generate-response
+                                   (fn [error]
+                                       (pr-str {:error error}))))
+
 
 (def generate-ast-response (partial generate-response
                                     (fn [data]
                                       (pr-str {:ast (:result data)}))))
+
+(defn readexpr [bstr] 
+     (binding [*read-eval* false] (read-string bstr)))
 
 (defroutes handler
   (GET "/" [] (resp/redirect "/index.html"))
@@ -54,15 +62,22 @@
        (generate-js-response {:result name}))
 
   (ANY "/compile" [expr]
-        (generate-js-response (cljs/compilation expr :simple false)))
+        (generate-js-response 
+            (cljs/compilation expr :simple false)
+              ))
+
+  (ANY "/compile-string" [expr]
+        (try
+          (generate-js-response 
+            (cljs/compilation (readexpr expr) :simple false))
+          (catch Exception e
+            (generate-js-error-response (.toString e))
+            )))
 
   (POST "/ast" [expr]
         (generate-ast-response (cljs/analyze expr :simple true)))
 
   (route/resources "/"))
-
-(defn readexpr [bstr] 
-     (binding [*read-eval* false] (read-string bstr)))
 
 (defroutes gethandler
 
